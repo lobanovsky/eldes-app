b981a4418171416c368a29b81508d3893a4ee77d
Холина Дарья Анатольевна <holina.darya@otr.ru>
2025-05-07 15:04:32 +0300

    Мелкие фиксы

--- src/views/eldes/index.tsx
+++ src/views/eldes/index.tsx
@@ -1,14 +1,15 @@
 import {useCallback, useEffect, useState} from "react";
+import {useSelector} from "react-redux";
 import styled from 'styled-components';
 import { Card, CardContent, Typography} from "@mui/material";
-import {useSnackbar} from 'notistack';
-import {UserDevices, getDevices, Device, AreaType} from "./services";
+import {UserDevices, getDevices} from "./services";
 import {GateOpenButton} from "./open-button";
+import {Loading} from "components/loading";
 import {FlexBox} from "components/styled";
-import {useSelector} from "react-redux";
-import {getAuth} from "../../store/auth/selectors";
-import {Loading} from "../../components/loading";
-import {useNotifications} from "../../hooks";
+import {useNotifications} from "hooks";
+import {getAuth} from "store/auth/selectors";
+
+
 
 
 const ZoneCard = styled(Card)`
@@ -16,7 +17,6 @@ const ZoneCard = styled(Card)`
     max-width: 420px;
     width: 100%;
     background-color: rgba(255, 255, 255, 0.5) !important;
-    //height: 100%;
 `
 
 export const EldesController = () => {
@@ -53,7 +53,7 @@ export const EldesController = () => {
     return (
         // <div>
         // @ts-ignore
-        <FlexBox flex-direction='column' className='eldes view' style={{minHeight: 'calc(100% - 110px)', paddingBottom: '1em'}}>
+        <FlexBox flex-direction='column' className='eldes view' style={{minHeight: 'calc(100% - 1em)', paddingBottom: '1em'}}>
             {loading && <Loading text='Загружаем шлагбаумы'/>}
             {data.zones.map((zone) =>
                 <ZoneCard variant='outlined' key={zone.id}>
