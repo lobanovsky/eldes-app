b13e38ca0fcb15f35bbe520335b71c7ffb4a396b
Холина Дарья Анатольевна <holina.darya@otr.ru>
2025-05-08 14:14:27 +0300

    Выравниваем зоны по центру

--- src/views/eldes/index.tsx
+++ src/views/eldes/index.tsx
@@ -53,7 +53,7 @@ export const EldesController = () => {
     return (
         // <div>
         // @ts-ignore
-        <FlexBox flex-direction='column' className='eldes view' style={{minHeight: 'calc(100% - 1em)', paddingBottom: '1em', boxSizing: 'border-box'}} gap={'1.5em'}>
+        <FlexBox flex-direction='column' justify-content='center' className='eldes view' style={{minHeight: 'calc(100% - 1em)', paddingBottom: '1em', boxSizing: 'border-box'}} gap={'48px'}>
             {loading && <Loading text='Загружаем шлагбаумы'/>}
             {data.zones.map((zone) =>
                 <ZoneCard variant='outlined' key={zone.id}>
